f327fb9724ae79d476728cc3c0551acc871b0843
osb15 <ombhamare51@gmail.com>
2025-01-26 15:40:20 +0530

    setup done

---
+++ src/main/java/com/gdl/Grace/Dental/Orders/GraceDentalOrdersApplication.java
@@ -0,0 +1,15 @@
+package com.gdl.Grace.Dental.Orders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraceDentalOrdersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraceDentalOrdersApplication.class, args);
+		System.out.println("Application Started ....");
+		
+	}
+
+}
---
+++ src/test/java/com/gdl/Grace/Dental/Orders/GraceDentalOrdersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdl.Grace.Dental.Orders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraceDentalOrdersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
